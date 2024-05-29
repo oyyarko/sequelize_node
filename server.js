3044a60e508f5498a70f3e1b1ccf1715c699aa42
arko <arvinakori7@gmail.com>
2024-05-29 12:31:13 +0530

    Socket setup

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const db = require('./models');
-const userRoute = require("./routes/routes")
+const db = require("./models");
+const userRoute = require("./routes/routes");
+const { app, server } = require("./socket/socket");
 
-const app = express();
+// const app = express();
 
 const PORT = process.env.PORT;
 
@@ -25,18 +26,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // to test routes
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello");
+// });
 
 // Sync database and start the server
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+db.sequelize
+  .sync()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
   });
-}).catch(err => {
-  console.error('Unable to connect to the database:', err);
-});
 
 // configure routes for APIs
 app.use("/", userRoute);
---
+++ socket/socket.js
@@ -0,0 +1,51 @@
+const { Server } = require("socket.io");
+const {createServer} = require("http");
+const express = require("express");
+const { join } = require('path');
+
+const app = express();
+const server = createServer(app);
+// const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:8080",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  },
+});
+
+// app.get('/', (req, res) => {
+//   res.sendFile(join(__dirname, '../index.html'));
+// });
+
+const getReceiverSocketId = (receiverId) => {
+  return userSocketMap[receiverId];
+};
+
+const userSocketMap = {}; // {userId: socketId}
+
+io.on("connection", (socket) => {
+  // console.log("user connected socket.id :>> ", socket.id);
+  // socket.broadcast.emit('hi');
+  // socket.on('chat message', (msg) => {
+  //   console.log('message: ' + msg);
+  // });
+  const userId = socket.handshake.query.userId;
+  if (userId != "undefined") userSocketMap[userId] = socket.id;
+
+
+  // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected :>> ", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+module.exports = { app, io, server, getReceiverSocketId };
