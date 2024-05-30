b01fba3870732379c053b110c572886cb955b82c
Arvina Kori <arvinakori7@gmail.com>
2024-05-30 11:13:41 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DBNAME,
     "host": process.env.DB_HOST, 
-    "dialect": "mysql"
+    "dialect": "mysql",
     "dialectModule": mysql2,
     "logging": true
   },
